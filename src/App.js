6bea790ba0d9af9f4fd5afea37ee3dd33fb3d429
Maxime <maxime.rawinski@gmail.com>
2020-04-01 17:52:58 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react'
 import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        avatar="https://randomuser.me/api/portraits/lego/6.jpg"
-        name="Ratata Gonzales"
-        online
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/lego/0.jpg"
-        name="Novak Gounvaski"
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/lego/4.jpg"
-        name="Joe Pompéleup"
-        online
-      />
+      <ContactList />
     </div>
   )
 }
