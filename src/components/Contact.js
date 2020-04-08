cd33cbb5b8b67e327bc43335df98bd21f28a6ef5
Maxime <maxime.rawinski@gmail.com>
2020-04-08 11:14:30 +0200

    react5 done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,47 @@ import React from "react";
 import "./Contact.css";
 import PropTypes from "prop-types";
 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
+  }
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt="" />
+
+        <div className="">
+          <h4 className="name">{this.props.name}</h4>
+          <div className="status">
+            <div
+              className={this.state.online ? "status-online" : "status-offline"}
+              onClick={(event) => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline });
+              }}
+            />
+            <p className="status-text">
+              {this.props.online ? "online" : "offline"}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  online: PropTypes.string.isRequired,
+};
+
+export default Contact;
+
+/*
 function Contact(props) {
   return (
     <div className="Contact">
@@ -17,11 +58,4 @@ function Contact(props) {
     </div>
   );
 }
-
-Contact.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  online: PropTypes.string.isRequired,
-};
-
-export default Contact;
+*/
